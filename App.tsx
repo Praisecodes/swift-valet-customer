43246e56776328ec7bb326fc9c19c7b36b2a2ecc
Praisecodes <cmancoder@gmail.com>
2024-06-19 11:42:42 +0100

    chore: setup fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "sora": require("./assets/fonts/Sora-Regular.ttf"),
+    "sora-medium": require("./assets/fonts/Sora-Medium.ttf"),
+    "sora-semibold": require("./assets/fonts/Sora-SemiBold.ttf"),
+    "sora-bold": require("./assets/fonts/Sora-Bold.ttf"),
+    "sora-light": require("./assets/fonts/Sora-Light.ttf"),
+    "cabinet": require("./assets/fonts/CabinetGrotesk-Regular.otf"),
+    "cabinet-bold": require("./assets/fonts/CabinetGrotesk-Bold.otf"),
+    "cabinet-medium": require("./assets/fonts/CabinetGrotesk-Medium.otf"),
+    "cabinet-light": require("./assets/fonts/CabinetGrotesk-Light.otf"),
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className={`flex-1 justify-center items-center`}>
+      <Text className={`font-sora-medium`}>Open up App.tsx to start working on your app!</Text>
       <Text>
         Environment: {process.env.EXPO_PUBLIC_ENVIRONMENT}
       </Text>
@@ -12,12 +27,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sora": ["sora"],
+        "sora-medium": ["sora-medium"],
+        "sora-bold": ["sora-bold"],
+        "sora-light": ["sora-light"],
+        "sora-semibold": ["sora-semibold"],
+        "cabinet": ["cabinet"],
+        "cabinet-bold": ["cabinet-bold"],
+        "cabinet-medium": ["cabinet-medium"],
+        "cabinet-light": ["cabinet-light"],
+      }
+    },
   },
   plugins: [],
 }
