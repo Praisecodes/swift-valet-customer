a72d5962dbb33749c000c3e29d991fb5fdbef66e
Praisecodes <cmancoder@gmail.com>
2024-06-22 20:55:30 +0100

    feat: build home page UI

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,9 +1,26 @@
 import { Text, View, Image, TouchableWithoutFeedback } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Avatar from '../../../assets/images/Avatar.png';
 import Hamburger from '../../../assets/icons/hamburger.svg';
+import Close from '../../../assets/icons/close.svg';
+import Settings from '../../../assets/icons/settings.svg';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 const Header = () => {
+  const animatedHeight = useSharedValue<any>(0);
+  const [navOpen, setNavOpen] = useState<boolean>(false);
+  const animatedHeightStyle = useAnimatedStyle(() => ({
+    height: animatedHeight.value
+  }));
+
+  useEffect(() => {
+    if (navOpen) {
+      animatedHeight.value = withTiming(123, { duration: 90 });
+    } else {
+      animatedHeight.value = withTiming(0, { duration: 90 });
+    }
+  }, [navOpen]);
+
   return (
     <View className={`flex flex-row items-center justify-between`}>
       <View className={`flex flex-1 flex-row gap-x-3 items-center mr-4`}>
@@ -11,15 +28,38 @@ const Header = () => {
           source={Avatar}
         />
 
-        <Text className={`font-sora text-base`}>
-          Hello Okeypraise!!
+        <Text className={`font-sora text-[17px]`} numberOfLines={1}>
+          Hello, Okeypraise!!
         </Text>
       </View>
 
-      <View>
-        <TouchableWithoutFeedback>
-          <Hamburger />
+      <View className={`relative`}>
+        <TouchableWithoutFeedback onPress={() => { setNavOpen(true) }}>
+          <Hamburger width={40} height={40} />
         </TouchableWithoutFeedback>
+
+        <Animated.View
+          className={`absolute overflow-hidden z-30 bg-white w-[270px] top-0 right-0 rounded-md`}
+          style={[{ elevation: 0.5 }, animatedHeightStyle]}
+        >
+          <View className={`px-4 border-b border-b-[#E4E4E443] py-3 flex flex-row items-center justify-end`}>
+            <TouchableWithoutFeedback onPress={() => { setNavOpen(false) }}>
+              <View className={`p-1.5`}>
+                <Close />
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+
+          <TouchableWithoutFeedback>
+            <View className={`px-4 py-3 flex flex-row gap-x-3 items-center`}>
+              <Settings width={45} height={45} />
+
+              <Text className={`font-sora-medium text-lg`}>
+                Settings
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
+        </Animated.View>
       </View>
     </View>
   )
--- src/screens/app/home/index.tsx
+++ src/screens/app/home/index.tsx
@@ -1,12 +1,28 @@
-import { Image, ScrollView, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { ScrollView, Text, TouchableWithoutFeedback, View } from 'react-native';
 import React from 'react';
 import Header from '../../../components/home/header';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { AppScreensStackParamList } from '../../../routes';
+import ScanCard from '../../../components/home/scan_card';
+import Plus from '../../../../assets/icons/plus.svg';
 
-const Home = () => {
+const Home = ({ navigation }: { navigation: BottomTabNavigationProp<AppScreensStackParamList, "home"> }) => {
   return (
     <View className={`flex-1 bg-white`}>
-      <ScrollView contentContainerStyle={{ minHeight: "100%", position: "relative", paddingHorizontal: 18, paddingVertical: 20 }}>
+      <ScrollView contentContainerStyle={{ minHeight: "98.4%", position: "relative", paddingHorizontal: 18, paddingVertical: 26 }}>
         <Header />
+        <ScanCard />
+        <View className={`mt-4`}>
+          <Text className={`font-sora-bold text-lg`}>
+            List of Personal cars
+          </Text>
+        </View>
+
+        <TouchableWithoutFeedback>
+          <Text className={`p-5 bg-white rounded-full absolute shadow-md bottom-2.5 right-3`} style={{elevation: 6, shadowColor: "#8A8A8A64"}}>
+            <Plus />
+          </Text>
+        </TouchableWithoutFeedback>
       </ScrollView>
     </View>
   )
